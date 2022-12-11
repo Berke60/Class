4f15b07a4baa4b9bcd0c4985c497aa544ad94760
vatan <vatan @LAPTOP-SU8E7320>
2022-12-11 16:17:54 +0300

    Signed-off-by: vatan  <vatan @LAPTOP-SU8E7320>

---
+++ src/CustomerManager.java
@@ -0,0 +1,19 @@
+
+public class CustomerManager {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void Add() {
+		System.out.println("Müşteri eklendi");
+	}
+
+	public void Remove() {
+		System.out.println("Müşteri silindi");
+	}
+
+	public void Update() {
+		System.out.println("Müşteri güncellendi");
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+			
+	public static void main(String[] args) {
+		//reference type
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+	}
+	
+	
+}
